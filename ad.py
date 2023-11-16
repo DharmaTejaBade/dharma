874e2fc1c152e4a8454b918db8a4b8a7a65c1d00
DharmaTejaBade <151020903+DharmaTejaBade@users.noreply.github.com>
2023-11-16 17:25:41 +0530

    Update ad.py

--- ad.py
+++ ad.py
@@ -1,3 +1,10 @@
-print("nani")
-print(int(input()))
-j = 8
+k = int(input())
+f = 0 
+for i in range(2,k):
+    if k%i ==0:
+        f+=1
+if f == 0:
+    print("{} is prime number".format(k))
+else:
+    print("{} is not a prime number".format(k))
+
